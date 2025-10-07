c3e7e358adc886557bb4b3a8e25c10ff26e9e101
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-07 14:28:18 +0200

    print "Buzz" for multiples of 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,9 @@ for (let i = 1; i <= 100; i++) {
     if (i % 3 === 0) {
         // per i multipli di 3 stampi “Fizz”
         console.log("Fizz");
+    } else if (i % 5 === 0) {
+        // per i multipli di 5 stampi “Buzz”
+        console.log("Buzz");
     } else {
         // per i numeri restanti
         console.log(i);
